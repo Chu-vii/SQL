a49e07fbe0266ca4a73123226c328fb75c6ced87
Victoria Chuprina <106590100+Chu-vii@users.noreply.github.com>
2022-11-21 18:45:18 +0200

    Add section "Запросы корректировки"

--- MySQL tasks.sql
+++ MySQL tasks.sql
@@ -294,7 +294,54 @@ from
     WHERE date_step_end IS NOT NULL AND step_id = 1
     group by book_id) as main
 group by title   
-order by sum(Сумма) desc
+order by sum(Сумма) desc;
+
+--ЗАПРОСЫ КОРРЕКТИРОВКИ
+
+/* 1. Включить нового человека в таблицу с клиентами. Его имя Попов Илья, его email popov@test, проживает он в Москве.*/
+
+insert into client(name_client, city_id, email)
+values ('Попов Илья',(select city_id from city
+where name_city = 'Москва'),'popov@test');
+
+/* 2. оздать новый заказ для Попова Ильи. Его комментарий для заказа: «Связаться со мной по вопросу доставки».
+
+Важно! В решении нельзя использоваться VALUES и делать отбор по client_id*/
+
+INSERT INTO buy (buy_description, client_id)
+SELECT 'Связаться со мной по вопросу доставки', client_id FROM client WHERE name_client='Попов Илья';
+
+/* 3. Создать счет (таблицу buy_pay) на оплату заказа с номером 5, в который включить название книг, их автора, цену, количество заказанных книг и  стоимость. Последний столбец назвать Стоимость. Информацию в таблицу занести в отсортированном по названиям книг виде.*/
+
+create table buy_pay AS
+SELECT title, name_author, price, buy_book.amount, price*buy_book.amount as Стоимость
+FROM author inner join book using (author_id)
+            inner join buy_book using (book_id)
+WHERE buy_book.buy_id = 5
+order by title;
+
+/* 4. Создать общий счет (таблицу buy_pay) на оплату заказа с номером 5. Куда включить номер заказа, количество книг в заказе (название столбца Количество) и его общую стоимость (название столбца Итого).  Для решения используйте ОДИН запрос.*/
+
+create table buy_pay as
+select buy_id, sum(buy_book.amount) as Количество, sum(buy_book.amount*price) as Итого
+from book inner join buy_book using (book_id)
+where buy_id=5;
+
+/* 5. Завершить этап «Оплата» для заказа с номером 5, вставив в столбец date_step_end дату 13.04.2020, и начать следующий этап («Упаковка»), задав в столбце date_step_beg для этого этапа ту же дату.
+Реализовать два запроса для завершения этапа и начале следующего. Они должны быть записаны в общем виде, чтобы его можно было применять для любых этапов, изменив только текущий этап. Для примера пусть это будет этап «Оплата».*/
+
+UPDATE buy_step bs1,
+       buy_step bs2
+SET bs1.date_step_end = '2020-04-13',
+    bs2.date_step_beg = '2020-04-13'
+WHERE bs1.buy_id = 5
+      AND bs2.buy_id = bs1.buy_id
+      AND bs1.step_id = (SELECT step_id FROM step WHERE name_step = 'Оплата')
+      AND bs2.step_id = bs1.step_id + 1;
+
+SELECT * FROM buy_step
+WHERE buy_id = 5;
+
 
 
 
